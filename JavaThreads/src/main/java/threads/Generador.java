93d5f4fe7e71e7b1a597667a69641f2a890f7cec
Karina <kserranoa16@gmail.com>
2020-10-23 18:41:58 -0600

    Declarando variables

--- JavaThreads/src/main/java/threads/Generador.java
+++ JavaThreads/src/main/java/threads/Generador.java
@@ -3,5 +3,10 @@ package threads;
 
 
 public class Generador {
-
+    Array rapidez[] = poca(90), normal(60), intemerdia(40), rapida(30);
+    int watt = 100;
+    int cantGen; 
+    
+    // if rapidez {establecerVel}
+    // calcEnergia = rapidez * watt * cantGen; 
 }
