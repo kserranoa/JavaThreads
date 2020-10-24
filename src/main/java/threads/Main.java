2a689ad8f8e77743173e86f54b07c4001d4ad2f8
kserranoa <67085518+kserranoa16@users.noreply.github.com>
2020-10-24 16:53:12 -0600

    Probar conexion repositorio remoto

---
+++ src/main/java/threads/Main.java
@@ -0,0 +1,13 @@
+//Prueba de conexion entre git-local y git-remote 
+//Por favor, al crear un repositorio para JAVA, por favor, agregar el pom.xml gracias
+
+package threads;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+      
+    }
+
+}
