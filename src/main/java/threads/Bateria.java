c9869956048362873a69a061bcc3e144dd63bbe8
kserranoa <67085518+kserranoa16@users.noreply.github.com>
2020-10-25 14:21:32 -0600

    Agregar comentarios

---
+++ src/main/java/threads/Bateria.java
@@ -0,0 +1,14 @@
+/*
+La empresa cuenta con una batería capaz de almacenar 10 GW
+*/
+package threads;
+
+
+public class Bateria {
+    int capMax; 
+    int cantBaterias;
+    int cantCarga;
+    
+    // if (cantCarga < 10000){
+// calcEnergia} 
+}
