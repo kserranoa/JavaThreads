19e51fccf96f94195df01c7eec12f512650e0070
kserranoa <67085518+kserranoa16@users.noreply.github.com>
2020-10-25 14:21:26 -0600

    Agregar comentarios

---
+++ src/main/java/threads/Clima.java
@@ -0,0 +1,15 @@
+/*
+El clima va a cambiar cada 2 minutos
+*/
+package threads;
+
+
+public class Clima {
+    double tiempo; 
+    double random = Math.random();
+    double velocidad; 
+    
+    public void establecerVel(){
+        //2 minutos
+    }
+}
