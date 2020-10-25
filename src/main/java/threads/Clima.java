68fc3b85129464a1455bfb1669d07a2c3caea162
kserranoa <67085518+kserranoa16@users.noreply.github.com>
2020-10-25 16:47:56 -0600

    Numero aleatorio de la velocidad

--- src/main/java/threads/Clima.java
+++ src/main/java/threads/Clima.java
@@ -2,14 +2,20 @@
 El clima va a cambiar cada 2 minutos
 */
 package threads;
-
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class Clima {
-    double tiempo; 
-    double random = Math.random();
     double velocidad; 
     
     public void establecerVel(){
-        //2 minutos
+        /*
+        De manera aleatoria va escoger una velocidad del clima (rapido 30, intermedio 40, normal 60 y lento 90)
+        */
+         List<Integer> lista = Arrays.asList(30, 40, 60, 90);
+        Random aleatorio = new Random();
+        int r = lista.get(aleatorio.nextInt(lista.size()));
+        double velocidad = r;
     }
 }
